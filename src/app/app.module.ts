67ebd70be3e199b5a221e027bf904eeeff3b64fc
Yunior Gonzalez <yunior@squaads.com>
2020-11-06 19:07:41 +0000

    Base del proyecto creada
    Co-authored-by: BRYAN TAMAYO ABAD <bryan.tamayo101@alu.ulpgc.es>
    Co-authored-by: Lau Sarmiento <laumsm93@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './views/hello/hello.component';
+import { SebasComponent } from './components/sebas/sebas.component';
+import { OsamaComponent } from './components/osama/osama.component';
+import { BelenComponent } from './belen/belen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    SebasComponent,
+    OsamaComponent,
+    BelenComponent
   ],
   imports: [
     BrowserModule,
